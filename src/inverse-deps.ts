ca77334bcfd91913d0f5e8761f3611d297f0ae6f
Sonny, 성천 <new.sungchuni@gmail.com>
2023-04-24 02:03:10 +0900

    chore: set inverse deps return types explicitly

--- src/inverse-deps.ts
+++ src/inverse-deps.ts
@@ -1,7 +1,7 @@
 type Deps<T> = Map<keyof T, Set<keyof T>>
 
-export const inverseDeps = <T>(deps: Deps<T>): Deps<T> =>
-  Array.from(deps.entries()).reduce<Map<keyof T, Set<keyof T>>>(
+export const inverseDeps = <T>(deps: Deps<T>) =>
+  Array.from(deps.entries()).reduce<Deps<T>>(
     (accumulator, [property, deps]) => {
       deps.forEach((dep) => {
         accumulator.set(dep, (accumulator.get(dep) || new Set()).add(property))
